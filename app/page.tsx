d1d0d7df8610ebd0ad5c077bce70553407136585
chinmaypaliwal1 <96851687+chinmaypaliwal1@users.noreply.github.com>
2024-11-27 20:06:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,21 @@
 import { useEffect, useState } from "react";
 import HomeSection from "./about/HomeSection";
 
+type User = {
+  id: number;
+  name: string;
+};
+
 export default function Home() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     fetch("/api/users")
       .then((res) => res.json())
       .then((data) => {
         if (data.success) {
-          setUsers(data.data);
+          setUsers(data.data); 
           console.log(data);
-          
         }
       });
   }, []);
@@ -20,6 +24,16 @@ export default function Home() {
   return (
     <div>
       <HomeSection />
+      <div className="mt-8">
+        <h2 className="text-2xl font-semibold">Users List:</h2>
+        <ul className="mt-4 space-y-2">
+          {users.map((user) => (
+            <li key={user.id} className="p-2 bg-gray-100 rounded shadow">
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
